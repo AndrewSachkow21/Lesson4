7d2cb1c9817f826a1ead91fbe0304c1b10e59a76
github_username <bravlerkrutenko@gmail.com>
2024-10-19 17:41:19 +0300

    edit

--- taskb.py
+++ taskb.py
@@ -7,6 +7,3 @@ def list_remove(N,list,P):
         list2[P-2:int(N)]=[list2[P-2]]
     return [the_r]+list2
 print(list_remove(input("1"),input("2"),int(input("3"))))
-b = (i for i in range(2))
-print(next(b))
-print(next(b))
