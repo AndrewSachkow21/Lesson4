f3b521c61ffbedfbc0ad1f255722229e3104ff02
github_username <bravlerkrutenko@gmail.com>
2024-10-30 21:23:44 +0300

    update and add tasks

--- taskD.py
+++ taskD.py
@@ -2,9 +2,10 @@ def middle_mark(b):
     result = 0
     for i in b:
         result += int(i)
-    return result/len(b)
+    return round(result/len(b),2)
 def ocinka(d,*b):
     list = [b[i].split() for i in range(d)]
     dict_marks={v[0]:middle_mark(v[1: ]) for v in list}
+    print(dict_marks)
     return dict_marks
-print(ocinka(2,"ivanov 5 4 6","danichkin 3 4 5"))
+print(ocinka(2,"ivadov 5 4 1","danichkin 3 4 5"))
---
+++ з.py
@@ -0,0 +1,4 @@
+b=["d","b3","b","g","g","n","b2"]
+a=(x+"ee" for x in b if "b" in x)
+yield:
+print(next(a),next)
