d6b301b9089c987f9c7b2227b71d7cc59512c4be
github_username <bravlerkrutenko@gmail.com>
2024-11-02 17:04:46 +0300

    update

--- taskA.py
+++ taskA.py
@@ -1,8 +1,2 @@
-def list_remove(N,list,P):
-    list2 = list.split()
-    output = list2.copy()
-    for i in list2:
-        if int(i) == P:
-            output.remove(i)
-    return output
-print(list_remove(input("1"),input("2"),int(input("3"))))
+G=input("3")
+print([x for x in input("2").split() if int(x) != int(G)])
